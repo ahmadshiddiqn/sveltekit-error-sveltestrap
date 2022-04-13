ec00956a1d7f29225d64a9c875af67dc02d8781e
Ahmad Shiddiq Nurrahman <asn1998@gmail.com>
2022-04-13 10:14:00 +0700

    commit error reproduction

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import { Col, Container, Row } from 'sveltestrap';
+</script>
+  <Row>
+    <Col sm="12" md={{ size: 6, offset: 3 }}>
+        <h1>Welcome to SvelteKit</h1>
+        <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+    </Col>
+  </Row>
